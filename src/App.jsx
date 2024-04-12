b33ffaa911c40842a62109d9964a9c01a2987142
dom-torcelino <torcelino.johndominic@gmail.com>
2024-04-12 15:45:20 +0800

    Final Update
    HIW
    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import HowItWorks from "./components/HowItWorks";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
-
 import * as Sentry from "@sentry/react";
 
 const App = () => {
@@ -15,7 +15,8 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
-      {/* <HowItWorks /> */}
+      <HowItWorks />
+      <Footer />
     </main>
   );
 };
